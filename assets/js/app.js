897afe2408592145ac47ec1b989b32834b620d6c
ARIELOZAM <arielloza71@gmail.com>
2023-08-10 01:40:42 -0600

    Bran New DEsign Portfolio}

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,19 @@
-function onSection() {
-	const sections = document.querySelectorAll("section");
-	console.log(sections)
+function darkMode() {
+const darkModeBtn = document.querySelector("#darkModeSwitch"); 
+darkModeBtn.addEventListener("click", function() {
+// p and heading elements should change to white
+const paragraphs = document.querySelectorAll(p); // Select all p elements
+const headingOne = document.querySelectorAll(h1); // Select all h1 elements
+const headingTwo = document.querySelectorAll(h2); // Select all h2 elements
+const headingThree = document.querySelectorAll(h3); // Select all h3 elements
 
-	for(let i = 0; i < sections.length ; i++) {
-		console.log(sections[i].id)
-		if(sections[i].id == "") {
-			sections[i].setAttribute("data", "no-id");
-				if(sections[i].id == "") {
-					sections[i].setAttribute("class", "bg-danger");
-				}
-		}
-		else {
-		sections[i].setAttribute("class", "hv-100 bg-dark");
-	}
-	}
-}
+	paragraphs.toggleClass("dark-mode-text");
+	
 
-onSection();
+});
 
-
-function formatStyles() {
-	const components = [
-			{
-				headingOne: document.querySelectorAll("h1"),
-				headingTwo: document.querySelectorAll("h2"),
-			}
-		]
-	for(let i = 0; i < components.length ; i++) {
-		console.log(components[i]);
-		
-		for(let h = 0; h < components[i].headingOne.length ; h++) {
-			const h1 = components[i].headingOne[h];
-		h1.setAttribute("class", "text-danger")
-		}
-	}
+	
 }
 
 
-function arielozamAPI() {
-const data = [{
-		devData: {
-			name: "ARIELOZAM",
-			role: "Front End Developer"
-		}
-}];
-}
+darkMode();
